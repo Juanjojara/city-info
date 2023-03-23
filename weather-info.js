d5192b5e7def9680d6b36c675fbd25c94dbdf40b
j.jaralaconich <J.JaraLaconich@almaviva.it>
2023-03-23 12:28:27 +0100

    Deleted commented code

--- weather-info.js
+++ weather-info.js
@@ -17,15 +17,6 @@ async function addWeatherInfo(city){
             reject({ "error": error })
         });
     })
-    /*await axios.get('https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=8d95d90347337455ac1ccd658337b2aa', config)
-    .then(function(response) {
-        console.log(response.data.weather[0])
-        //console.log(response.status)
-        //console.log(response.statusText)
-        //console.log(response.headers)
-        //console.log(response.config)
-        return "city"
-    });*/
 
 }
 
